6f7f0f96499feec7137ff99cb3e1fe8840401497
Jay veer <jayveeronpath@gmail.com>
2024-12-22 00:17:48 +0530

    create Tweet models and register in admin site.

--- X Clone/src/tweet/admin.py
+++ X Clone/src/tweet/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Tweet
 
 # Register your models here.
+@admin.register(Tweet) # this is a decorator to register the model with the admin site. 
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ['user', 'text', 'created_at'] # this is to display the fields in the admin site. 
+    search_fields = ['text'] # this is to search the fields in the admin site by text field or any other fields.
+    list_filter = ['created_at'] # this is to filter the fields in the admin site by created_at field or any other fields. 
+    date_hierarchy = 'created_at' # this is to display the data hierarchy in the admin site by created_at field or any other fields.
+
+# admin.site.register(Tweet, TweetAdmin) # this is the same as the about code but without the decorator but this is not recommended because it is not clear that this is a model admin class above code is more clear and recommended way to do this. 
--- X Clone/src/tweet/models.py
+++ X Clone/src/tweet/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=500)
+    photo = models.ImageField(upload_to='tweet_photos/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True) # set the time when the tweet was created 
+    updated_at = models.DateTimeField(auto_now=True) # set the time when the tweet was updated 
+
+    def __str_(self):
+        return f'{self.user.username} - {self.text[:10]}'
+
